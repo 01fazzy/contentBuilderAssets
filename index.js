b73c122a47bbe4a19d34fa3a58a6a981bf66826e
01fazzy <faizal@cyntexa.com>
2020-12-09 11:01:48 +0530

    Commit3

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ app.use(express.urlencoded({
   extended: true
 }))
 
+
+app.post('/stack',function(req,res){
+  var tmp = req.body.SourceClientID;
+  var tmp1 = req.body.SourceClientSecret;
+  var tmp2 = req.body.SourceMID;
+
+  console.log(tmp)  // stackoverflow0
+  console.log(tmp1)  // stackoverflow1
+  console.log(tmp2)
+})
+
+
+
+
+
+
+
 app.listen(port, () => {
    console.log('Example app is listening on port http://localhost:${port}');
 });
